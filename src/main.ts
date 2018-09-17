caafef330fb47379b1a7e18e38731a1dd5bd436d
Stuart Williamson <stuartw@phase2online.com>
2018-09-16 21:15:24 -0500

    init project

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+import Amplify from 'aws-amplify';
+import awsmobile from './aws-exports';
+
+Amplify.configure(awsmobile)
+Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
