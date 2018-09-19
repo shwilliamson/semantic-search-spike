f9c21a554e3f71ee44ed2ebaf5bc83f0e6ca24f1
Stuart Williamson <stuartw@phase2online.com>
2018-09-18 22:09:23 -0500

    add auth component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +8,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmplifyAngularModule
   ],
-  providers: [],
+  providers: [
+    AmplifyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import Amplify from 'aws-amplify';
-import awsmobile from './aws-exports';
+import awsexports from './aws-exports';
 
-Amplify.configure(awsmobile)
+Amplify.configure(awsexports)
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
 
 if (environment.production) {
