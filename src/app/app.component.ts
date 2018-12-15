4bbb8c61e7f744330d9c6439afe393bf779dbc42
Stuart Williamson <stuartw@phase2online.com>
2018-12-15 12:36:22 -0600

    Listen to auth events and add search component with routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Hub } from 'aws-amplify';
+import { Router } from '@angular/router';
+import { RouterInitializer } from '@angular/router/src/router_module';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'semantic-search-spike';
+  router = null;
+
+  constructor(router: Router) {
+    this.router = router
+    Hub.listen('auth', this, 'authListener');
+  }
+
+  onHubCapsule(capsule) {
+      switch (capsule.payload.event) {
+        case 'signIn':
+          this.router.navigateByUrl('/search');
+          break;
+        case 'signUp':
+        this.router.navigateByUrl('/search');
+          break;
+        case 'signOut':
+        this.router.navigateByUrl('/');
+          break;
+        case 'signIn_failure':
+          break;
+        case 'configured':
+          break;
+      }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular'
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'search', component: SearchComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AmplifyAngularModule
+    AmplifyAngularModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AmplifyService
